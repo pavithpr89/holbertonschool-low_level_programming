97421ab6059610904c157cc64818802cb2ea11fb
pavithpr89 <pavithpr89@gmail.com>
2025-10-17 03:37:02 +0000

    Did the first exercise

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,12 +18,9 @@ last_digit = n % 10;
 if (last_digit < 0)
 last_digit *= -1;
 
-if (last_digit > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-else if (last_digit == 0)
-printf("Last digit of %d is %d and is 0\n", n, last_digit);
-else
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is %s\n", n, last_digit,
+(last_digit > 5) ? "greater than 5" :
+(last_digit == 0) ? "0" : "less than 6 and not 0");
 
 return (0);
 }
