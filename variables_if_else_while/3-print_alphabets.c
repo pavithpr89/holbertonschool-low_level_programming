cbac60d38dad69c4249ca27a194a0c802aa65fd9
pavithpr89 <pavithpr89@gmail.com>
2025-10-18 03:08:58 +0000

    Did the third exercise

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase,
+ *        followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+
+return (0);
+}
