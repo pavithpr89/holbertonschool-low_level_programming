d1b1ee42faef34c9803a143a29471e0744999cc3
pavithpr89 <pavithpr89@gmail.com>
2025-10-18 03:42:09 +0000

    Did the last exercise

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -14,17 +14,17 @@ for (i = 0; i <= 8; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-putchar(i + '0');
-putchar(j + '0');
+putchar(i + 48);
+putchar(j + 48);
 if (i != 8 || j != 9)
 {
-putchar(',');
-putchar(' ');
-			}
+putchar(44);  /* ASCII for ',' */
+putchar(32);  /* ASCII for space ' ' */
+}
 }
 }
 
-putchar('\n');
+putchar(10);  /* ASCII for newline '\n' */
 
 return (0);
 }
