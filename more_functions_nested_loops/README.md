759062e536444dc98f69c33aad516fd17aefd2b3
pavithpr89 <pavithpr89@gmail.com>
2025-10-20 23:31:36 +0000

    Did the 1st exercise

--- more_functions_nested_loops/README.md
+++ more_functions_nested_loops/README.md
@@ -2,5 +2,9 @@
 Author:  Pavith Rajapakse
 
 task 1 : Write a function that checks for uppercase character.
+task 2 : Write a function that checks for a digit (0 through 9)
+
+
+
 
 
--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -3,11 +3,10 @@
 
 int main(void)
 {
-	char c;
-
-	c = 'A';
-	printf("%d\n", _isupper(c));  /* should print 1 */
-	c = 'a';
-	printf("%d\n", _isupper(c));  /* should print 0 */
+	printf("%d\n", _isdigit('5')); /* 1 */
+	printf("%d\n", _isdigit('a')); /* 0 */
+	printf("%d\n", _isdigit('0')); /* 1 */
+	printf("%d\n", _isdigit('9')); /* 1 */
+	printf("%d\n", _isdigit('x')); /* 0 */
 	return (0);
 }
