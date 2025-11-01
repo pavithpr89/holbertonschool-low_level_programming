ee2e5b9bcb48098fdabd9790de6fbae855bc9524
pavithpr89 <pavithpr89@gmail.com>
2025-11-01 00:23:10 +0000

    did 4th exercise

--- argc_argv/README.md
+++ argc_argv/README.md
@@ -5,6 +5,8 @@ task 1 : Write a program that prints its name, followed by a new line
 task 2 : Write a program that prints the number of arguments passed into it
 task 3 : Write a program that prints all arguments it receives
 task 4 : Write a program that multiplies two numbers
+task 5 : Write a program that adds positive numbers
+
 
 
 
--- argc_argv/main.c
+++ argc_argv/main.c
@@ -1,13 +1,17 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	printf("Test 1 (2 * 3):\n");
-	system("./mul 2 3");
-	printf("\nTest 2 (10 * 5):\n");
-	system("./mul 10 5");
-	printf("\nTest 3 (wrong args):\n");
-	system("./mul 5");
+	printf("Test 1: no arguments\n");
+	system("./add");
+	printf("\nTest 2: 1 2 3\n");
+	system("./add 1 2 3");
+	printf("\nTest 3: 5 10 20\n");
+	system("./add 5 10 20");
+	printf("\nTest 4: invalid argument 3a\n");
+	system("./add 3a 5");
+	printf("\nTest 5: single number 42\n");
+	system("./add 42");
 	return (0);
 }
