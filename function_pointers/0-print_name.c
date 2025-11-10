c7d76a36cb300f2df064a16bb778f5bd39f09e81
pavithpr89 <pavithpr89@gmail.com>
2025-11-10 23:41:03 +0000

    Did the 0 exercise

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: pointer to function that prints a string
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,4 @@
+#Description of the functions pointers folder with decriptions of each scripts
+Author:  Pavith Rajapakse
+
+task 0  : Write a function that prints a name.
