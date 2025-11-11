15e81d9f525f10d8134b2d1f588acd00708828c9
pavithpr89 <pavithpr89@gmail.com>
2025-11-11 23:39:49 +0000

    Did the 3rd exercise

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*op_func)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+op_func = get_op_func(argv[2]);
+if (op_func == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+result = op_func(num1, num2);
+printf("%d\n", result);
+return (0);
+}
