c463cdd955d1641be9567d844cf4932384ff231d
pavithpr89 <pavithpr89@gmail.com>
2025-11-25 07:06:55 +0000

    First exercise

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-/* Your list node structure */
+/* Linked list node structure */
 typedef struct list_s
 {
     char *str;
@@ -12,35 +12,11 @@ typedef struct list_s
 } list_t;
 
 /* Function prototypes */
-size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
 
-#endif#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdlib.h>
-#include <unistd.h>
-
-/**
- * struct list_s - singly linked list
- * @str: string (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
-
-#endif
+#endif /* LISTS_H */
