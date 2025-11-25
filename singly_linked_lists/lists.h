bc3f9d6bd4190acc343d8bc728f2996b36926548
pavithpr89 <pavithpr89@gmail.com>
2025-11-25 08:47:05 +0000

    did the 4th

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,11 +2,10 @@
 #define LISTS_H
 
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string - malloc'd string
+ * @str: string
  * @len: length of the string
  * @next: points to the next node
  */
@@ -18,8 +17,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+void free_list(list_t *head);  /* <-- add this line */
 
 #endif /* LISTS_H */
