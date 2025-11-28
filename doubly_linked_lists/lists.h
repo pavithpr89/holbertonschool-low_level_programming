3f98d24fcaa7aec10a8053aaf218a4f67453b7b4
pavithpr89 <pavithpr89@gmail.com>
2025-11-28 23:57:19 +0000

    did the 1st

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -9,8 +9,6 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
- * Description: doubly linked list node structure
  */
 typedef struct dlistint_s
 {
@@ -20,5 +18,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif
