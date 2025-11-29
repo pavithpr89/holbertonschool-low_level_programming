5f0f8122b999cb0737388a2019faa32c9dcb5876
pavithpr89 <pavithpr89@gmail.com>
2025-11-29 04:16:48 +0000

    Did the 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes text_content to it
+ * @filename: the name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t w;
+size_t len = 0;
+
+if (!filename)
+return (-1);
+
+/* Open file: O_CREAT to create, O_WRONLY to write, O_TRUNC to truncate */
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+
+/* If text_content is not NULL, calculate its length and write it */
+if (text_content)
+{
+while (text_content[len])
+len++;
+
+w = write(fd, text_content, len);
+if (w != (ssize_t)len)
+{
+close(fd);
+return (-1);
+}
+}
+
+close(fd);
+return (1);
+}
