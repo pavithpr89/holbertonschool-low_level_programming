092419cbd06b92ba5fad8eea0b5db8e162937fc6
pavithpr89 <pavithpr89@gmail.com>
2025-12-01 02:22:07 +0000

    Did the 2nd

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,13 +1,15 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
- * @key: key, string (unique)
- * @value: corresponding value
- * @next: pointer to next node in the list
+ * @key: The key (string). It is unique in the hash table.
+ * @value: The value corresponding to the key.
+ * @next: A pointer to the next node in the list (for collisions).
  */
 typedef struct hash_node_s
 {
@@ -17,9 +19,9 @@ typedef struct hash_node_s
 } hash_node_t;
 
 /**
- * struct hash_table_s - Hash table structure
- * @size: size of the array
- * @array: array of linked list pointers
+ * struct hash_table_s - Hash table data structure
+ * @size: The size of the array
+ * @array: An array of pointers to the first node of each bucket
  */
 typedef struct hash_table_s
 {
@@ -27,6 +29,13 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* Mandatory tasks */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
